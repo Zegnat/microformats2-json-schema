67a9668c23d765849de2015af2434b2c1537c36d
Martijn van der Ven <martijn@vanderven.se>
2021-12-25 23:31:37 +0100

    Update dependencies and test runner

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
+const { readFileSync } = require('fs')
 const { sync } = require('fast-glob')
 const test = require('ava')
-const Ajv = require('ajv')
 
-const ajv = new Ajv()
-const validator = ajv.compile(JSON.parse(require('fs').readFileSync('schema.json')))
+const ajv = new (require('ajv'))()
+require("ajv-formats")(ajv, ["uri"])
+const validator = ajv.compile(JSON.parse(readFileSync('schema.json')))
 
 function tester(t, json) {
-  t.true(validator(JSON.parse(require('fs').readFileSync(json))))
+  t.true(validator(JSON.parse(readFileSync(json))))
 }
 
 for (const json of sync('tests/testsuite/tests/**/*.json')) {
